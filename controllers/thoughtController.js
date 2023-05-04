51c6ee65c4df93e25d2d7ad76b0734c0db39501e
mbatorek7 <maegan.batorek@valpo.edu>
2023-05-04 16:16:51 -0500

    working on thought controller

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,34 +1,54 @@
-const { User, Thought } = require('../models/User');
+const { User, Thought } = require('../models');
 
 module.exports = {
-    //get all users
+    //get all thoughts
     async getThought(req, res) {
         try {
-            
+            const thoughts = await Thought.find();
+            res.json(thoughts);
         } catch(err) {
-            
+            res.status(500).json(err);
         }
     },
-    //get a single user
+    //get a single thought
     async getOneThought(req, res) {
         try {
-            
+            const thought = await Thought.findOne({ _id: req.params.thoughtId })
+            .select('-__v');
+
+            if (!thought) {
+                return res.status(404).json({ message: 'No thought with that ID' });
+            }
+
+            res.json(thought);
         } catch(err) {
-            
+            res.status(500).json(err);
         }
     },
-    //create a user
+    //create a thought
     async createThought(req, res) {
         try {
-            
+            const thought = await Thought.create(req.body);
+            res.json(thought);
         } catch(err) {
-            
+            console.log(err);
+            return res.status(500).json(err);
         }
     },
-    //delete a user
+    //delete a thought
     async deleteThought(req, res) {
         try {
-            
+            const thought = await Thought.findOneAndDelete({ _id: req.params.thoughtId });
+
+            if (!thought) {
+                res.status(404).json({ message: 'No thought with that ID' });
+            }
+
+            const user = await User.findOneAndUpdate(
+                { _id: req.params.thoughtId },
+                { $pull: { thoughts: req.params.thoughtId } },
+                { new: true }
+            );
         } catch (err) {
             
         }
